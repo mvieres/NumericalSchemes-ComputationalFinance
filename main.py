5179a76130a0a383cd5fc4f187260ba6800e0ea6
faggl <m.4esfd@gmail.com>
2023-05-14 01:54:11 +0200

    erster plot

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from functions import brownian_motion
 
 
-T=5 # Time horizon
+T=1 # Time horizon
 N=1000 # Discretization number per time unit
-print(T)
+delta_t = T/N
+time = [_*delta_t for _ in range(N+1)]
+BB = brownian_motion(T=T,N=N)
+
+plt.plot(time,BB)
+plt.show()
