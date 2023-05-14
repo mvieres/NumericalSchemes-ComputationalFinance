9655e14b0336523db2d47aef0bf334fc48974881
faggl <m.4esfd@gmail.com>
2023-05-15 00:52:54 +0200

    Class Update

--- functions.py
+++ functions.py
@@ -1,28 +1,39 @@
 import numpy as np  
 
 
-def brownian_motion(n,N):
-    """Computes #N Sample paths of brownian motion
+class Market():
+    def __init__(self,n,N,sigma,mu,r,s0,T):
+        self.n = n
+        self.N = N
+        self.mu = mu
+        self.sigma = sigma
+        self.r = r
+        self.s0 = s0
+        self.T = T
+        
+    def brownian_motion(self):
+        """Computes #N Sample paths of brownian motion
 
-    Args:
-        n (_type_): _description_
-        N (_type_): _description_
-    """
-    delta_t = 1/n
-    mu, sigma = 0, np.sqrt(delta_t)
-    B = np.zeros(shape=(N,n+1))
-    for j in range(N):
-        Rv = np.random.normal(mu,sigma, n)
-        for i in range(n):
-            B[j,i+1] = B[j,i] + Rv[i]
-    return B
-
-
-def black_scholes(sigma,mu,s0,N,n):
-    time = np.linspace(0,1,n+1)
-    BB = brownian_motion(n,N)
-    S = np.zeros(shape=(N,n))
-    for j in range(N):
-        for i in range(n):
-            S[j,i] = s0*np.exp((mu - 0.5*(sigma**2))*time[i] + sigma*BB[j,i])
-    return S
+        Args:
+            n (_type_): _description_
+            N (_type_): _description_
+        """
+        t = self.time_grid()
+        delta_t = t[1] - t[0]
+        dB = np.sqrt(delta_t) * np.random.normal(size=(self.N,self.n - 1))
+        B0 = np.zeros(shape=(self.N,1))
+        B = np.concatenate((B0,np.cumsum(dB,axis=1)),axis= 1)
+        return B
+    
+    def black_scholes(self):
+        t = self.time_grid()
+        BB = self.brownian_motion()
+        S = np.zeros(shape=(self.N,self.n))
+        for j in range(self.N):
+            for i in range(self.n):
+                S[j,i] = self.s0*np.exp((self.mu - 0.5*(self.sigma**2))*t[i] + self.sigma*BB[j,i])
+        return S
+    
+    def time_grid(self):
+        time = np.linspace(0,self.T,self.n)
+        return time
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import brownian_motion, black_scholes
+from functions import Market
 
 
 T=1 # Time horizon
-n=100000 # Discretization number per time unit
-N = 10 # Number of paths
+n=1000 # Discretization number per time unit
+N = 5 # Number of paths
+sigma = 3
+mu = 0.5
+s0 = 1
+r= 0.01
 delta_t = T/n
-time = np.linspace(0,1,n)
-BB = brownian_motion(n=n,N=N)
-BS = black_scholes(2,3,1,N,n)
 
+#BB = Market.brownian_motion(n=n,N=N)
+M = Market(n=n,N=N,sigma=sigma,mu=mu,r=r,s0=s0,T=T)
+t = M.time_grid()
+BB = M.brownian_motion()
+Stock = M.black_scholes()
 for i in range(N):
-    plt.plot(time,BS[i,])
+    plt.plot(t,Stock[i,:])
 plt.legend()
 plt.show()
