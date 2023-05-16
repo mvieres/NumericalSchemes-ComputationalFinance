3f538e0aabc92e3fb15b28b1150848f562e82f30
faggl <m.4esfd@gmail.com>
2023-05-16 19:34:39 +0200

    meanreturn

--- functions.py
+++ functions.py
@@ -170,121 +170,13 @@ class Monte_Carlo():
         ki = np.array([lower, upper])
         return p_mc, var_mc, ki   
         
-        
-        
 
 
-
-
-
-
-# Numerical Methods
-
-def boundary_cond(x,L):
-    if x<=(L/2):
-        y = 2*x
-    else:
-        y = 2*(L - x)
-    return y
-
-
-
-def fd_forward_heat(Nx, Nt, T, L, sigma):
-    """Computes the forward finite differences approach for u_t = sigma^2 * u_xx
-
-    Args:
-        Nx (int): Discretization points for space
-        Nt (int): Discretization points for time
-        T (float): Time horizon
-        L (float): Space horizon
-        sigma (float): sigma coeff for heat equation
+def MeanReturn(mat):
+    """computes relative mean return of given Sample paths
+        (Matrix)
+        Each row equals one sample path, e.g. 
     """
-    # Parameters
-    delta_t = T / Nt
-    delta_x = L / Nx
-    nu = (sigma**2)*(delta_t/(delta_x**2))
-    u = np.zeros((Nt+1, Nx+1))
-    
-    # Boundary conditions (E2)
-    for j in range(Nx+1):
-        u[0,j] = boundary_cond(j*delta_x, L)
-        #u[0,j] = np.sin(j*delta_x)
-       
-    for i in range(Nt+1):
-        u[i,0] = 0
-        u[i,-1] = 0
-        
-    # Recursion for inner points
-    for i in range(Nt):
-        for j in range(1,Nx):
-            u[i+1, j] = nu*u[i, j-1] + (1-2*nu)*u[i, j] + nu*u[i, j+1]
-        
-    return u
-
-def fd_backward_heat(Nx, Nt, T, L, sigma):
-    """performs finite difference scheme for heat eqaution u_t = sigma^2 * u_xx
-
-    Args:
-        Nx (int): Discretization points for space
-        Nt (int): Discretization points for time
-        T (float): Time horizon
-        L (float): Space horizon
-        sigma (float): sigma coeff for heat equation
-    """
-    delta_t = T / Nt
-    delta_x = L / Nx
-    nu = (sigma**2)*(delta_t/(delta_x**2))
-    # Loesungsvektor u: Lenght  = N_x - 1
-    u = np.zeros((Nx, Nt))
-    
-    # Matrix B allocation mit dim (N_x, N_x)
-    B = np.zeros((Nx-1, Nx-1))
-    for i in range(Nx-1):
-        B[i,i] = 1+2*nu
-        
-    for i in range(Nx-2):
-        B[i+1,i] = -nu
-        B[i,i+1] = -nu
-
-    # L U Decomposition, Jetzt "R-U Zerlegung", da L obere Grenze vom State space ist
-    # Gleiche dim wie bei B
-    U, R = lu(B,permute_l=True)
-    U_inv = np.linalg.inv(U)
-    R_inv = np.linalg.inv(R)
-    
-
-    
-    # Allocate U_0
-    for j in range(1, Nx):
-        u[j,0] = boundary_cond(j*delta_x,L)
-        #u[j,0] = np.sin(j*delta_x)
-    # Randbedingungen (fuer t) sind hier egal, da diese konstant 0 sind und mit 0 initialisiert wird
-        
-    # Rekursion
-    for i in range(Nt-1):  
-        u_vec = u[1:,i]
-        y = U_inv.dot(u_vec)
-        u[1:,i+1] = R_inv.dot(y)
-    return u
-    
-       
-def real_sol(t, x):
-    n = 10000
-    u = 0
-
-    for i in range(1,n+1):
-        u = u + ((-1)**(i-1))*np.exp((-(2*i-1)**2)*t)*(np.sin((2*i-1)*x))*((2*i-1)**(-2))
-    u = (8/np.pi)*u
-
-    return u
-
-def get_max_error(delta_x,u_approx,L):
-    """
-    get error at t = 0.1 for simple heat equation
-    delta_t 
-    """
-    for j in range(len(u_approx)):
-        u_t = real_sol(0.1,L*j/delta_x)
-    diff = abs(u_t - u_approx)
-    error = diff.max()
-    return error
+    abs_return = np.mean(mat,axis=1) # Axis = 1 import
+    relative_return = (abs_return - mat[0,0])/mat[0,0]
+    return relative_return
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import Market
+from functions import Market, MeanReturn
 
 
 T=1 # Time horizon
 n=1000 # Discretization number per time unit
 N = 5 # Number of paths
-sigma = 3
-mu = 0.5
+sigma = 0.5
 s0 = 1
-r= 0.01
+r= 0.05
 delta_t = T/n
 
 #BB = Market.brownian_motion(n=n,N=N)
-M = Market(n=n,N=N,sigma=sigma,mu=mu,r=r,s0=s0,T=T)
+M = Market(n=n,N=N,sigma=sigma,r=r,s0=s0,T=T)
 t = M.time_grid()
 BB = M.brownian_motion()
 Stock = M.black_scholes()
+
+test = MeanReturn(Stock)
+print(test)
 for i in range(N):
     plt.plot(t,Stock[i,:])
 plt.legend()
