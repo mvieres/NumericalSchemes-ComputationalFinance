6ba10908ee69ad5814c06908f86248e31fef2ef6
faggl <m.4esfd@gmail.com>
2023-05-16 20:10:38 +0200

    added discount factor

--- functions.py
+++ functions.py
@@ -81,15 +81,19 @@ class European_Options():
         self.S = Assetprice
         self.t = t
     
-    def Arithmetic_asian_call(self):
+    def Arithmetic_asian_call(self,discounted):
         """Computes an UNDISCOUNTED European Arithmetic asian Call given the underlying asset S and strike price K
 
         Returns:
             Vector / Array: Value of the option
         """
-        Value = np.zeros(shape = (self.N,))
-        for j in range(self.N):
-           Value[j] = np.max((1/self.n)* np.sum(self.S[j,:]) - self.K , 0) 
+        Value = np.zeros(shape = (self.N,))           
+        if discounted == True:
+            for j in range(self.N):
+                Value[j] = np.exp(-self.r*(self.t[-1] -self.t[0]))*np.max((1/self.n)* np.sum(self.S[j,:]) - self.K , 0) 
+        else:
+            for j in range(self.N):
+                Value[j] = np.max((1/self.n)* np.sum(self.S[j,:]) - self.K , 0) 
         return Value
     
     def Call(self,discounted):
@@ -102,7 +106,7 @@ class European_Options():
 
         if discounted == True:
             for j in range(self.N):
-                Value[j] = np.exp(-self.r*(t[-1] -t[0]))*np.max(0, self.S[j,-1] - self.K)
+                Value[j] = np.exp(-self.r*(self.t[-1] -self.t[0]))*np.max(0, self.S[j,-1] - self.K)
         else:
             for j in range(self.N):
                 Value[j] = np.max(0, self.S[j,-1] - self.K)
@@ -118,22 +122,28 @@ class European_Options():
         Value = np.zeros(shape = (self.N,))
         if discounted == True:
             for j in range(self.N):
-                Value[j] = np.exp(-self.r*(t[-1] -t[0]))*np.max(0,  self.K - self.S[j,-1])
+                Value[j] = np.exp(-self.r*(self.t[-1] -self.t[0]))*np.max(0,  self.K - self.S[j,-1])
         else:
             for j in range(self.N):
                 Value[j] = np.max(0,  self.K - self.S[j,-1])
         return Value
     
-    def geo_asian_call(self):
-        """Computes an UNDISCOUNTED European geometric asian call given the underlying asset S and strike price K
+    def geo_asian_call(self,discounted):
+        """Computes an European geometric asian call given the underlying asset S and strike price K
 
         Returns:
             Array: Value of option
         """
         Value = np.zeros(shape=(self.N,))
-        for j in range(self.N):
-           Value[j] = np.max(0, (np.prod(self.S)**(1/self.n)) - self.K) 
+
+        if discounted == True:
+            for j in range(self.N):
+                Value[j] = np.exp(-self.r*(self.t[-1] - self.t[0]))*np.max(0, (np.prod(self.S)**(1/self.n)) - self.K) 
+        else:
+            for j in range(self.N):
+                Value[j] = np.max(0, (np.prod(self.S)**(1/self.n)) - self.K) 
         return Value
+    
 
 
 
