Still wrong results

algo.py
@@ -4,7 +4,7 @@ from options import value_fun_x as vfun
 
 
 #
-def LSM(Market,degree):
+def LSM(Market,degree,K):
 
     Stock = Market.black_scholes()
     t = Market.time_grid()
@@ -17,20 +17,21 @@ def LSM(Market,degree):
     S = np.zeros(shape=(m,n))
     
     for j in range(m):
-        if vfun().Call(x = Stock[j,n-1]):
+        if vfun().Call(x = Stock[j,n-1],K=K):
             S[j,n-1] = 1
     
     for j in range(m):
-        C[j,n-1] = vfun().Call(x= Stock[j,n-1])
+        C[j,n-1] = vfun().Call(x= Stock[j,n-1],K=K)
     
     
-    for j in range(n, 2, -1):
+    for j in range(n-1, 3, -1):
+        print(j)
         U = np.zeros(shape=(m,))
         z = 0 
         Xtemp = np.array([])
         Ytemp = np.array([])
         for i in range(m):
-            if vfun().Call(x = Stock[i,j-1]) > 0:
+            if vfun().Call(x = Stock[i,j-1],K=K) > 0:
                 U[i] = 1
                 val = np.array(np.sum([C[i,k]+np.exp(-r*(t[k+1]-t[k])) for k in range(j,n-1)]))
                 Ytemp = np.append(Ytemp, val)
@@ -38,25 +39,26 @@ def LSM(Market,degree):
                 z = z + 1
     
 
-    X = Xtemp
-    Y = Ytemp
-    regression = np.polyfit(X,Y,degree)
-    Xcont = np.polyval(regression, Stock[:,j])
-    Xex = np.array([vfun().Call(x = X[_]) for _ in range(len(X))])
-    
-    z = 0 # Nicht klar wo dieses Init hingehoert
-    for i in range(m):
+        X = Xtemp
+        Y = Ytemp
+        regression = np.polyfit(X,Y,degree)
+        Xcont = np.polyval(regression, Stock[:,j])
+        Xex = np.array([vfun().Call(x = X[_],K=K) for _ in range(len(X))])
         
-        if U[i] == 1: # Checkin for in the money path
-            if Xex[z] < Xcont[z]:
-                for k in range(j,n): # n Evnetuell falsch
-                    C[i,k] = S[i,k]*vfun().Call(x = Stock[i,k])
-            else:
-                S[i, j-1] = 1
-                S[i, j in range(n)] = 0
-                C[i, j-1] = vfun().Call(x = Stock[i,j-1])# U.u viele indexfehler
-                C[i, j in range(n)] = 0
-        z = z+1
+        
+        for i in range(m):
+            
+            z = 0 # Nicht klar wo dieses Init hingehoert
+            if U[i] == 1: # Checkin for in the money path
+                if Xex[z] < Xcont[z]:
+                    for k in range(j,n): # n Evnetuell falsch
+                        C[i,k] = S[i,k]*vfun().Call(x = Stock[i,k],K=K)
+                else:
+                    S[i, j-1] = 1
+                    S[i, j in range(n)] = 0
+                    C[i, j-1] = vfun().Call(x = Stock[i,j-1],K=K)# U.u viele indexfehler
+                    C[i, j in range(n)] = 0
+            z = z+1
     
     # Monte Carlo (?)
     Value = np.sum(np.sum(C*np.exp(-r+delta_t),axis=1),axis=0)/m

options.py
@@ -113,8 +113,8 @@ class value_fun_x():
     def __init__(self) -> None:
         pass
     
-    def Call(self,x): 
-        K = 1  
+    def Call(self,x,K): 
+        
         return np.maximum(0, x - K)
     
     def Put(self,x,K):

test_algo.py
@@ -1,23 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from market import Market
 from algo import LSM
 
 
 T = 1
-N = 4
-n = 3
+N = 1000
+n = 100
 
 degree = 10
 
 r = 0.01
-sigma = 0.5
+sigma = 0.4
 s0 = 5
 M = Market(n=n,N=N,sigma=sigma,r=r,s0=s0,T=T)
 t = M.time_grid()
 BB = M.brownian_motion()
 Stock = M.black_scholes()
 
-
-Value, S, C = LSM(Market= M,degree = degree)
-print(Value)
+Value = np.zeros(shape=(9,))
+for k in range(1, 10, 1):
+    Value[k-1], S, C = LSM(Market= M,degree = degree,K = 3)
+    
+plt.plot(Value)
+plt.show()