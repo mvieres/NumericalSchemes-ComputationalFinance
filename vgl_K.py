b55d0e14fc51e46081c8f70dfa758dd5987c89c3
faggl <m.4esfd@gmail.com>
2023-06-05 09:17:47 +0200

    update picture

--- vgl_K.py
+++ vgl_K.py
@@ -7,7 +7,7 @@ from scipy.stats import norm
 
 T = 1
 N = 10000
-n = 1000
+n = 100
 
 
 degree = 5
@@ -37,17 +37,17 @@ for k in K:
     d1 = (np.log(s0/k)+ (r + 0.5*sigma**2)*T)
     d2 = d1 - sigma*np.sqrt(T)
     value_e = s0*norm.cdf(d1) - k*np.exp(-r)*norm.cdf(d2)
-
     res_theory[z] = value_e
+    
     #res_value[z] = LSM(Market=M,degree=degree,K=k)
     z = z+1
 
 plt.plot(K,res_value)
 plt.plot(K,res_theory)
-plt.title('LSM with 1000 MC Runs and Gridsize 100, s0 = 36')
+plt.title('LSM with '+ str(N)+ ' MC Runs and Gridsize '+str(n)+ ', s0 = '+str(s0))
 plt.axvline(s0,color = 'red')
 plt.xlabel('Strike price K')
 plt.ylabel('Estimated v_0')
-plt.savefig('picture.png')
 plt.legend(['Estimated Value', 'Theoretical Value', 's0'])
+plt.savefig('picture.png')
 plt.show()
