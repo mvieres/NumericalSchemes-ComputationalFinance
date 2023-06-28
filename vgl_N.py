4a537d96c3389e4aa209f8b6d30513f25f7c39c1
faggl <m.4esfd@gmail.com>
2023-06-28 23:48:54 +0200

    vgl_N.py Regression

--- vgl_N.py
+++ vgl_N.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from market import Market
 from algo import longstaff_schwartz
 from scipy.stats import norm
+from sklearn.linear_model import LinearRegression
 
 
 # Parameters
 T = 1
 K = 36
-n = 10
+n = 100
 degree = 4
 r = 0.06
 sigma = 0.2
@@ -33,9 +34,18 @@ d1 = (np.log(s0 / K) + (r + 0.5 * sigma ** 2) * T)
 d2 = d1 - sigma * np.sqrt(T)
 value_e = np.maximum(s0 * norm.cdf(d1) - K * np.exp(-r) * norm.cdf(d2), 0)
 diff = np.abs(res_value - value_e)
+
+# Regression
+N_res = N.reshape(-1,1)
+diff_res = diff.reshape(-1,1)
+lin_reg = LinearRegression()
+lin_reg.fit(N_res, diff_res)
+diff_pred = lin_reg.predict(N_res)
+
 print(res_value)
+print("Linear Regression coefficient:" + str(lin_reg.coef_))
 plt.plot(N, diff)
-print(diff)
+plt.plot(N, diff_pred, color='red')
 plt.title('LSM with strike price ' + str(K) + ' and Grid-size ' + str(n) + ', s0 = ' + str(s0))
 plt.xlabel('Number of Monte Carlo Runs')
 plt.ylabel('Difference')
