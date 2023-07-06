b0982aface4231a67ddbecab80ae4f6ec8dbf876
faggl <m.4esfd@gmail.com>
2023-07-06 20:14:44 +0200

    Extending Algo with Legendre Regression

--- algo.py
+++ algo.py
@@ -1,49 +1,52 @@
 import numpy as np
-from numpy.polynomial import legendre
+import scipy.special as ss
 
 
-
-def Call(x, K):
-    return np.maximum(0, x - K)
-
-
-def Put(x, K):
-    return np.maximum(0, K - x)
+def call(x, k):
+    return np.maximum(0, x - k)
 
 
-def Arithmetic_Asian_Call(x, K, n):
-    return np.max((1 / n) * np.sum(x - K), 0)
+def put(x, k):
+    return np.maximum(0, k - x)
 
 
-def longstaff_schwartz(Market, degree, K, payoff, regression_type):
-    """Performs the Least squares Monte Carlo Estimation for American Put(!!) Options. Uses Black Scholes Model
+def arithmetic_asian_call(x, k, n):
+    return np.max((1 / n) * np.sum(x - k), 0)
 
-    Args:
-        TODO: New doc
-        Market (Array): on axis 0: Sample paths, on axis 1 values at each time point for a given sample path
-        degree (int): Maximum polynomial degree to be considered for regression
-        K (float): strike price
-        payoff (string): Payoff function of Option
-        regression_type (string): Basis function to be used
-    Returns:
-        value_0 (float): Estimated value of american Option
-        var_mc (float): Monte Carlo estimate variance
+
+def lsmc(market, degree, k, payoff, regression_type):
+    """
+    Performs the least-squares-monte-carlo algorithm using all paths
+    @param market: Underlying market model
+    @param degree: maximum of degrees for regression
+    @param k: strike price
+    @param payoff: payoff structor of option
+    @param regression_type: regression to be used (polynomial / legendre)
+    @return: estimated value value_0, variance, RootSquaredMeanError
     """
 
-    # Extracting t, S from Market environment
-    t = Market.time_grid()
-    S = Market.black_scholes()
-    r = Market.r
-    N = Market.N
-    n = Market.n
+    # Extracting t, s from Market environment
+    t = market.time_grid()
+    s = market.black_scholes()
+    r = market.r
+    paths = market.N
+    n = market.n
+    delta_t = t[1] - t[0]  # delta time
+    num_steps = t.shape[0]  # Number of time steps (col-dimensions)
+    num_mc = s.shape[0]  # Number of monte carlo runs (row-dimensions)
+    discount = np.exp(-r * delta_t)  # Constant discount factor for equidistant grid
+    value = np.zeros_like(s)  # Pre-allocation for value
 
     # Processing input string of option payoff
     if payoff.lower() == "call":
-        def g(a): return Call(a, K)
+        def g(a):
+            return call(a, k)
     elif payoff.lower() == "put":
-        def g(a): return Put(a, K)
+        def g(a):
+            return put(a, k)
     elif payoff.lower() == "arithmetic_asian_call":
-        def g(a): return Arithmetic_Asian_Call(a, K=K, n=n)
+        def g(a):
+            return arithmetic_asian_call(a, k=k, n=n)
     else:
         print("No valid Option chosen")
         return
@@ -55,43 +58,33 @@ def longstaff_schwartz(Market, degree, K, payoff, regression_type):
     else:
         flag = False
 
-    delta_t = t[1] - t[0]  # delta time
-    num_steps = t.shape[0]  # Number of time steps (col-dimensions)
-    num_mc = S.shape[0]  # Number of monte carlo runs (row-dimensions)
-
-    discount = np.exp(-r * delta_t)  # Constant discount factor for equidistant grid
-    value = np.zeros_like(S)  # Pre-allocation for value
-
-    # Compute Value for Last Time point, i.e g()
-    for j in range(num_mc):
-        value[j, -1] = g(S[j, -1])
-
+    # Beginning of computation
+    value[:, -1] = g(s[:, -1])  # Compute Value for Last Time point
     # Iteration backwards in Time
     for t in range(num_steps - 2, -1, -1):
-
+        # Regression
         if flag:
-            # TODO: Fix Legendre
-            reg = legendre.legfit(S[:, t], value[:, t + 1] * discount, degree)[0]
+            reg = np.polynomial.legendre.legfit(s[:, t], value[:, t + 1] * discount, deg=degree)
+            continuation_value = np.zeros_like(value[:, -1])
+            for j in range(num_mc):
+                s_transformed_j = [ss.eval_legendre(deg, s[j, t]) for deg in range(degree+1)]
+                continuation_value[j] = np.dot(reg, s_transformed_j)
         else:
-            reg = np.polyfit(S[:, t], value[:, t + 1] * discount, deg=degree)
-        # TODO: ADD estimation of contin_val via neural network
-        contin_val = np.polyval(reg, S[:, t])
+            reg = np.polyfit(s[:, t], value[:, t + 1] * discount, deg=degree)
+            continuation_value = np.polyval(reg, s[:, t])
 
-        # Exercise value
-        ex_val = np.zeros_like(S[:, t])
-        for j in range(num_mc):
-            ex_val[j] = g(S[j, t])
+        # Compute exercise value
+        ex_val = np.zeros_like(s[:, t])
+        for j in range(num_mc):  # for loop structure because g takes only single values
+            ex_val[j] = g(s[j, t])
 
-        for j in range(num_mc):
-            if ex_val[j] >= contin_val[j]:
-                value[j, t] = g(S[j, t])
-            else:
-                value[j, t] = value[j, t + 1]
-
+        # Update Value Array
+        value[:, t] = np.where(ex_val > continuation_value, ex_val, value[:, t + 1] * discount)
         # Standard Monte Carlo 
-        v_0 = value[:, 0]  #* discount
+        v_0 = value[:, 0] * discount
         value_0 = np.mean(v_0)
         v0_var = np.var(v_0)
-        var_mc = v0_var / N
+        var_mc = v0_var / np.sqrt(paths)
+        rmse = np.sqrt(np.mean(((v_0 - value_0) / paths)**2))
 
-    return value_0, var_mc
+    return value_0, var_mc, rmse
--- testalgo.py
+++ testalgo.py
@@ -1,28 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 from market import Market
-from algo import longstaff_schwartz
+from algo import lsmc
 from scipy.stats import norm
 
 T = 1
-N = 10000
-n = 1000
+N = 100000
+n = 100
 
-degree = 5
+degree = 10
 
 r = 0.06
 sigma = 0.2
 s0 = 36
-K = 36
+K = 33
+payoff_structure = "call"
+reg = "legendre"
+#reg = "polynomial"
 
-M = Market(n=n, N=N, sigma=sigma, r=r, s0=s0, T=T)
+
+M = Market(n=n, paths=N, sigma=sigma, r=r, s0=s0, time_horizon=T)
 d1 = (np.log(s0/K) + (r + 0.5*sigma**2)*T)
 d2 = d1 - sigma*np.sqrt(T)
-value_e = s0*norm.cdf(d1) - K*np.exp(-r)*norm.cdf(d2)
+value_e = s0*norm.cdf(d1) - K*np.exp(-r)*norm.cdf(d2)  # Theoretical Values
 
-Value, var = longstaff_schwartz(Market=M, degree=degree, K=36, payoff="call", regression_type="polynomial")
+Value, var, _ = lsmc(market=M, degree=degree, k=K, payoff=payoff_structure, regression_type=reg)
 
-print('Estimated value v_0 American: ', Value)
+print('Estimated value at t=0 for American ' + str(payoff_structure) + ': ', Value)
 print('American Standard Variation: ', np.sqrt(var))
-print('European Call value: ', value_e)
+print('European theoretical CALL value: ', value_e)
