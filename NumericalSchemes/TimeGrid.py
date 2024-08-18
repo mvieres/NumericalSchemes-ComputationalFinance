10c705b9546532391038d33eedd2ee6cc8edab01
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-08-19 00:29:27 +0200

    Update TimeGrid.py

--- NumericalSchemes/TimeGrid.py
+++ NumericalSchemes/TimeGrid.py
@@ -4,25 +4,26 @@ import pandas as pd
 
 # TODO: This file needs work, i.e. TimeGrid is used for simple calculations on a gird. Everything else needs to be specified
 
+
 class LocalDate:
     def __init__(self, year: int, month: int, day: int):
         self.date = datetime.date(year, month, day)
         self.diffToPreviousDate = None
         self.diffToNextDate: float
 
-    def getDiffToPreviousDate(self) -> float:
+    def get_diff_to_previous_date(self) -> float:
         return self.diffToPreviousDate
 
-    def getDiffToNextDate(self) -> float:
+    def get_diff_to_next_date(self) -> float:
         return self.diffToNextDate
 
-    def setDiffToPreviousDate(self, previousDate: datetime.date) -> None:
+    def set_diff_to_previous_date(self, previousDate: datetime.date) -> None:
         self.diffToPreviousDate = (self.date - previousDate).days
 
-    def setDiffToNextDate(self, nextDate: datetime.date) -> None:
+    def set_diff_to_next_date(self, nextDate: datetime.date) -> None:
         self.diffToNextDate = (nextDate - self.date).days
 
-    def asOrdinal(self):
+    def as_ordinal(self):
         return self.date.toordinal()  # This is used to get diff w.r.t. days
 
 
@@ -31,15 +32,15 @@ class TimeGrid:
     TimeGrid creates an equidistant time grid. It stores different grids with respect to the number of points n.
     """
 
-    def __init__(self, tStart: float or datetime, tEnd: float or datetime):
-        self.tStart = tStart
-        self.tEnd = tEnd
+    def __init__(self, t_start: float or datetime, t_end: float or datetime):
+        self.tStart = t_start
+        self.tEnd = t_end
         self.__timegrid = {}  # integer as key, value is numpy array of time grid points
 
-    def computeTimeGrid(self, n: int) -> None:
+    def compute_time_grid(self, n: int) -> None:
         self.__timegrid[n] = np.linspace(self.tStart, self.tEnd, n)
 
-    def getTimeGrid(self, n=None) -> np.array:
+    def get_time_grid(self, n=None) -> np.array:
         """
         Returns the time grid with respect to the number of points n. If n is None, the complete dict of time-grids
         is returned.
@@ -47,26 +48,36 @@ class TimeGrid:
         if n is not None:
             assert isinstance(n, int), "n has to be an integer"
         if not self.__checkIfKeyAlreadyExists(n):
-            self.computeTimeGrid(n)
+            self.compute_time_grid(n)
         if n is None:
             return self.__timegrid
         else:
             return self.__timegrid[n]
 
-    def setTimeGrid(self, n: int, points: list[float]):
+    def set_time_grid(self, n: int, points: list[float]):
         assert not self.__checkIfKeyAlreadyExists(n), "Time grid already exists w.r.t. n points!"
         assert len(points) == n, "Number of points has to be equal to n"
-        assert max(list) < self.tEnd, "Time points have to be in the interval [tStart, tEnd]"
-        assert min(list) > self.tStart, "Time points have to be in the interval [tStart, tEnd]"
+        assert max(points) <= self.tEnd, "Time points have to be in the interval [tStart, tEnd]"
+        assert min(points) >= self.tStart, "Time points have to be in the interval [tStart, tEnd]"
         self.__timegrid[n] = np.array(points)
 
-    def getDtDiffToPreviousPoint(self, n: int, i: int) -> float:
+    def get_dt_diff_to_previous_point(self, n: int, i: int) -> float:
         assert i > 0, "i has to be bigger than 0"
+        if n not in self.__timegrid.keys():
+            self.initialize_time_grid_fallback(n)
         return self.__timegrid[n][i] - self.__timegrid[n][i - 1]
 
-    def getDtDiffToNextPoint(self, n: int, i: int) -> float:
+    def get_dt_diff_to_next_point(self, n: int, i: int) -> float:
         assert i < len(self.__timegrid[n])-1, "i has to be at most the first to last index"
+        if n not in self.__timegrid.keys():
+            self.initialize_time_grid_fallback(n)
         return self.__timegrid[n][i + 1] - self.__timegrid[n][i]
+
+    def initialize_time_grid_fallback(self, n: int) -> None:
+        """
+        Compute time grid if not initialized yet. Default is the equidistant time grid.
+        """
+        self.compute_time_grid(n)
 
     def __checkIfKeyAlreadyExists(self, n: int) -> bool:
         return n in self.__timegrid.keys()
@@ -89,7 +100,7 @@ class Calendar(TimeGrid):
         self.__simulationDates = simulationDates
         self.simulationCalender = None
 
-    def setTimeGrid(self, n: int):
+    def set_time_grid(self, n: int):
         pass
 
     def is_business_day(date):
--- NumericalSchemes/tests/test_TimeGrid.py
+++ NumericalSchemes/tests/test_TimeGrid.py
@@ -10,21 +10,35 @@ class TimeGridTest(unittest.TestCase):
 
     def test_computeTimeGrid(self):
         timeGridInstance = TG.TimeGrid(0, 1)
-        tenor = timeGridInstance.getTimeGrid(3)
+        tenor = timeGridInstance.get_time_grid(3)
         areEqual = np.array_equal(tenor, [0, 0.5, 1])
         self.assertTrue(areEqual)
         timeGridInstance = TG.TimeGrid(0, 2)
-        tenor = timeGridInstance.getTimeGrid(3)
+        tenor = timeGridInstance.get_time_grid(3)
         areEqual = np.array_equal(tenor, [0, 1, 2])
         self.assertTrue(areEqual)
+
+    def test_get_differences_equidistant(self):
+        timeGridInstance = TG.TimeGrid(0, 1)
+        self.assertEqual(timeGridInstance.get_dt_diff_to_previous_point(3, 1), 0.5)
+        self.assertEqual(timeGridInstance.get_dt_diff_to_previous_point(3, 2), 0.5)
+        self.assertEqual(timeGridInstance.get_dt_diff_to_next_point(3, 0), 0.5)
+
+    def test_get_differences_non_equidistant(self):
+        time_grid_instance = TG.TimeGrid(0, 10)
+        time_grid_instance.set_time_grid(3, [0, 3, 10])
+        self.assertEqual(time_grid_instance.get_dt_diff_to_previous_point(3, 1), 3)
+        self.assertEqual(time_grid_instance.get_dt_diff_to_previous_point(3, 2), 7)
+        self.assertEqual(time_grid_instance.get_dt_diff_to_next_point(3, 0), 3)
+        self.assertEqual(time_grid_instance.get_dt_diff_to_next_point(3, 1), 7)
 
     def testLD(self):
         date = TG.LocalDate(2020, 1, 1)
         date1 = TG.LocalDate(2021, 1, 1)
-        date.setDiffToPreviousDate(date.date)
-        self.assertEqual(date.getDiffToPreviousDate(), 0)
-        date.setDiffToNextDate(date.date)
-        self.assertEqual(date.getDiffToNextDate(), 0)
+        date.set_diff_to_previous_date(date.date)
+        self.assertEqual(date.get_diff_to_previous_date(), 0)
+        date.set_diff_to_next_date(date.date)
+        self.assertEqual(date.get_diff_to_next_date(), 0)
 
 
 if __name__ == '__main__':
