5025d81ff7ab6503939eb82595a008be4d76c842
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-08-20 22:20:13 +0200

    Update Brownian motion

--- NumericalSchemes/RandomProcesses.py
+++ NumericalSchemes/RandomProcesses.py
@@ -7,63 +7,66 @@ import NumericalSchemes.Utils as Utils
 class RandomProcesses:
 
     @staticmethod
-    def brownianMotionPath(timeGridInstance: TimeGrid, nSteps: int, startingPoint: list or float,
-                           correlationMatrix=None) -> np.array:
+    def brownian_motion_path(time_grid_instance: TimeGrid, n_steps: int, dimension: int, starting_point=None,
+                             correlation_matrix=None, seed=None) -> np.array:
         """
-        Create a Brownian motion path
-        @param timeGridInstance:
-        @param nSteps:
-        @param startingPoint:
-        @param correlationMatrix:
-        @param seed:
+        Create a Brownian motion path for a given time grid. If the time grid instance is not yet defined, equidistant
+        is chosen.
+        @param time_grid_instance:
+        @param n_steps:
+        @param dimension:
+        @param starting_point:
+        @param correlation_matrix:
         @return:
         """
-        assert nSteps >= 1
-        assert len(startingPoint) > 1 if correlationMatrix is not None else True
-        if correlationMatrix is not None:
-            assert len(startingPoint) == len(correlationMatrix), \
+        assert n_steps >= 1
+        assert len(starting_point) > 1 if correlation_matrix is not None else True
+        if correlation_matrix is not None:
+            assert len(starting_point) == len(correlation_matrix), \
                 "Dimension of starting point and correlation matrix must be equal"
-        sizeBB = len(startingPoint) if startingPoint is list else 1
-        timeGrid = timeGridInstance.getTimeGrid(nSteps)
-        delta_t = timeGrid[1] - timeGrid[0]
-        bronwianMotion = Utils.Utils.initForProcesses(startingPoint, nSteps)
-        bronwianMotion[0] = startingPoint
-        if correlationMatrix is not None:
-            L = np.linalg.cholesky(correlationMatrix)
-            for i in range(1, nSteps):
-                bronwianMotion[i] = (bronwianMotion[i - 1] + np.sqrt(delta_t) *
-                                     np.dot(L, np.random.normal(size=(len(startingPoint), 1))).T)
+        if starting_point is not None:
+            assert isinstance(starting_point, list) or starting_point is float, "Starting point must be a list or a float"
+            matrix = np.linalg.cholesky(correlation_matrix)
         else:
-            for i in range(1, nSteps):
-                bronwianMotion[i] = (bronwianMotion[i - 1] + np.sqrt(delta_t) *
-                                     np.random.normal(size=sizeBB))
+            matrix = np.eye(dimension)
+        brownian_motion = Utils.Utils.initForProcesses(dimension, n_steps)
+        if starting_point is not None:
+            brownian_motion[0] = starting_point
+        if seed is not None:
+            np.random.seed(seed)
 
-        return bronwianMotion
+        for i in range(1, n_steps):  # Loop over time steps
+            dt = time_grid_instance.get_dt_diff_to_previous_point(n_steps, i)
+            brownian_motion[i] = brownian_motion[i - 1] + np.sqrt(dt) * (matrix @ np.random.normal(size=dimension))
+        return brownian_motion
 
     @staticmethod
-    def brownianBridge(timeGridInstance: TimeGrid, nSteps1: int, nSteps2: int) -> np.array:
+    def brownian_bridge(time_grid_instance: TimeGrid, n_steps1: int, n_steps2: int) -> np.array:
         pass
 
     @staticmethod
-    def multipleBrownianMotionPaths(timeGridInstance: TimeGrid, nPaths: int, nSteps: int,
-                                    startingPoint, correlationMatrix=None) -> np.array:
+    def multiple_brownian_motion_paths(time_grid_instance: TimeGrid, n_paths: int, n_steps: int, dimension: int,
+                                       starting_point=None, correlation_matrix=None) -> np.array:
         """
         Create multiple Brownian motion paths
-        @param timeGridInstance:
-        @param nPaths:
-        @param nSteps:
-        @param startingPoint:
+        @param time_grid_instance:
+        @param n_paths:
+        @param n_steps:
+        @param dimension:
+        @param starting_point:
+        @param correlation_matrix:
         @return:
         """
-        bbMotionPaths = {}
-        for numberPath in range(nPaths):
-            bbMotionPaths[numberPath] = RandomProcesses.brownianMotionPath(timeGridInstance,
-                                                                            nSteps, startingPoint, correlationMatrix)
-        return bbMotionPaths
+        brownian_motion_paths = {}
+        for numberPath in range(n_paths):
+            brownian_motion_paths[numberPath] = RandomProcesses.brownian_motion_path(time_grid_instance, n_steps,
+                                                                                     dimension, starting_point,
+                                                                                     correlation_matrix)
+        return brownian_motion_paths
 
     @staticmethod
-    def geometricBrownianMotionExact(timeGridInstance: TimeGrid, nSteps: int, startingPoint: float, drift: float,
-                                     diffusion: float, seed: int = None) -> np.array:
+    def geometric_brownian_motion_exact(timeGridInstance: TimeGrid, nSteps: int, startingPoint: float, drift: float,
+                                        diffusion: float, seed: int = None) -> np.array:
         """
         Create a one dimensional geometric Brownian motion path
         @param timeGridInstance:
@@ -78,7 +81,7 @@ class RandomProcesses:
         assert diffusion >= 0, "Diffusion must be non negative"
         if seed is not None:
             np.random.seed(seed)
-        timeGrid = timeGridInstance.getTimeGrid(nSteps)
+        timeGrid = timeGridInstance.get_time_grid(nSteps)
         gbm = np.zeros((nSteps, 1))  # TODO: this might be the wrong shape
         for i in range(0, nSteps):
             gbm[i] = startingPoint*np.exp((drift - 0.5*diffusion**2)*timeGrid[i]
@@ -86,9 +89,9 @@ class RandomProcesses:
         return gbm
 
     @staticmethod
-    def multipleGeometricBrownianMotionExactPaths(timeGridInstance: TimeGrid, nPaths: int, nSteps: int,
-                                                  startingPoint: float, drift: float, diffusion: float,
-                                                  seed: int = None) -> np.array:
+    def multiple_geometric_brownian_motion_exact_paths(timeGridInstance: TimeGrid, nPaths: int, nSteps: int,
+                                                       startingPoint: float, drift: float, diffusion: float,
+                                                       seed: int = None) -> np.array:
         """
         Create multiple geometric Brownian motion paths
         @param timeGridInstance:
@@ -102,6 +105,6 @@ class RandomProcesses:
         """
         gbmPaths = {}
         for numberPath in range(nPaths):
-            gbmPaths[numberPath] = RandomProcesses.geometricBrownianMotionExact(timeGridInstance, nSteps, startingPoint,
-                                                                           drift, diffusion, seed)
+            gbmPaths[numberPath] = RandomProcesses.geometric_brownian_motion_exact(timeGridInstance, nSteps, startingPoint,
+                                                                                   drift, diffusion, seed)
         return gbmPaths
--- NumericalSchemes/tests/test_RandomProcesses.py
+++ NumericalSchemes/tests/test_RandomProcesses.py
@@ -8,22 +8,30 @@ import NumericalSchemes.TimeGrid as TimeGrid
 
 class RandomProcessesTest(unittest.TestCase):
 
-    def test_generateBrownianMotion(self):
+    def test_generate_bm_non_equidistant(self):
         np.random.seed(1)
-        timeGridInstance = TimeGrid.TimeGrid(0, 1)
-        random_movements = np.random.normal(size=10)
-        print(random_movements)
-        bb = RP.RandomProcesses.brownianMotionPath(timeGridInstance, 10, [0])
-        plt.plot(bb)
-        plt.show()
+        timeGridInstance = TimeGrid.TimeGrid(0, 3)
+        timeGridInstance.set_time_grid(3, [0, 1, 3])
+        random_movements = np.random.normal(size=2)
+        theoretical_value = np.array([0, np.sqrt(1)*random_movements[0], np.sqrt(1)*random_movements[0] + np.sqrt(2)*random_movements[1]])
+        bb = RP.RandomProcesses.brownian_motion_path(timeGridInstance, 3, dimension=1, seed=1)
+        self.assertTrue(np.array_equal(bb, theoretical_value))
 
     def test_generateMultipleBM(self):
         np.random.seed(100)
-        timeGridInstance = TimeGrid.TimeGrid(0, 1)
-        nPaths = 100
-        bb = RP.RandomProcesses.multipleBrownianMotionPaths(timeGridInstance, nPaths, 1000, [0])
-        for i in range(nPaths):
-            plt.plot(bb[i])
+        time_grid_instance = TimeGrid.TimeGrid(0, 1)
+        n_paths = 10
+        bb = RP.RandomProcesses.multiple_brownian_motion_paths(time_grid_instance, n_paths, 1000, 1)
+        for i in range(n_paths):
+            plt.plot(time_grid_instance.get_time_grid(1000), bb[i])
+        plt.show()
+
+    def test_multidimesnisonal_brownian_motion(self):
+        timeGridInstance = TimeGrid.TimeGrid(0, 10)
+        n_steps = 1000
+        bb = RP.RandomProcesses.brownian_motion_path(timeGridInstance, n_steps, 2)
+        print(bb)
+        plt.plot(bb[:, 0], bb[:, 1])
         plt.show()
 
 
