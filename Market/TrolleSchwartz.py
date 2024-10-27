1f33125a0ddaad1ccd7c8fdac056213bbc200b04
Maximilian Vieres <84975868+mvieres@users.noreply.github.com>
2024-10-27 15:48:23 +0100

    Trolle Schwartz

--- Market/TrolleSchwartz.py
+++ Market/TrolleSchwartz.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from Market.AbstractMarket import AbstractMarket
+from NumericalSchemes.SdeSolver import SdeSolver
 
 
 class TrolleSchwartz(AbstractMarket):
@@ -16,17 +17,39 @@ class TrolleSchwartz(AbstractMarket):
     simga_{f,i}(t,T) = (alpha_0 + alpha_1*(T-t))*exp(-gamma*(T-t)).
     # TODO: not clear if t_start has to be specified; paper implies interval [0,T]. Not clear if this curve viewd on [t,T] is the same as a simulation started at t.
     """
-    def __init__(self,t_start, t_end, alpha_0: float, alpha_1: float, gamma: float, kappa: float, theta: float, sigma: float, rho: float):
-        super().__init__(t_start, t_end, 10, 0.01)  # TODO: s0 is not used in the model -> review initialization
+    def __init__(self,t_start, t_end,r: float, alpha_0: float, alpha_1: float, gamma: float, kappa: float, theta: float, sigma: float, rho: float, scheme:str):
+        super().__init__(t_start, t_end, r, r)  # TODO: s0 is not used in the model -> review initialization
+        self.dimension = 2
+        self.scenarios = {}
+        self.scenarios = self.underlying
+        self.scheme = scheme
         self.alpha_0 = alpha_0
         self.alpha_1 = alpha_1
         self.gamma = gamma
         self.kappa = kappa
         self.theta = theta
         self.sigma = sigma
-        drift = {1: lambda t, x: self.x[1] * (self.alpha_0 +
-                                              self.alpha_1 * (self.t_end - t)) * np.exp(-self.gamma * (self.t_end - t)) * integral_term, 2: lambda t, x: 0}
-        pass
+        self.rho = rho
+        self.v0 = r * 0.1  # TODO: not clear if this is a good choice
+        self.drift_vol = lambda t, x: self.kappa*(self.theta - x[1])
+        self.diffusion_vol = {1: lambda t, x: self.rho*self.sigma*np.sqrt(x[1]), 2: lambda t, x: np.sqrt(1-rho**2)*sigma*np.sqrt(x[1])}
+        self.integral_term = lambda t, x: ((self.alpha_0 / self.gamma)*(1-np.exp(-self.gamma*(self.t_end-t)))
+                                     - (self.alpha_1 / self.gamma)*(self.t_end - t)*np.exp(-self.gamma*(self.t_end-t))
+                                     + (self.alpha_1 / self.gamma**2)*(1-np.exp(-self.gamma*(self.t_start-t))))
+        self.sigma_f = lambda t, x: (self.alpha_0 + self.alpha_1*(self.t_end-t))*np.exp(-self.gamma*(self.t_end-t))
+        self.drift_f = lambda t, x:  x[1]* self.sigma_f(t, x) * self.integral_term(t, x)
+        self.diffusion_f = {1: lambda t, x: self.sigma_f(t, x)*np.sqrt(x[1])}
+        self.drift = {1: self.drift_f, 2: self.drift_vol}
+        self.diffusion = {1: self.diffusion_f, 2: self.diffusion_vol}
+        solver_instance = SdeSolver(time_grid_instance=self.time_grid_instance, drift=self.drift,
+                                    diffusion=self.diffusion, starting_point=[self.r, self.v0], order_dimensions=[2, 1])
+        self.schemes = {'absolute_euler': solver_instance.absolute_euler}
+        assert scheme in self.schemes.keys(), f"Scheme {scheme} not implemented (or typo)"
 
-    def compute_solution_path(self, nSteps: int) -> np.array:
-        pass
+    def compute_solution_path(self, n_steps: int) -> np.array:
+        # TODO: absolute euler for first dimension implies that interest rate cannot be negative. This is i.a. not the case
+        return self.schemes[self.scheme](n_steps)
+
+    def generate_scenarios(self, n_paths: int, n_steps: int) -> None:
+        for i in range(n_paths):
+            self.scenarios[i] = self.compute_solution_path(n_steps)
--- Market/tests/test_TrolleSchwartz.py
+++ Market/tests/test_TrolleSchwartz.py
@@ -1,10 +1,50 @@
 import unittest
 
+from Market.TrolleSchwartz import TrolleSchwartz
 
-class test_TrolleSchwartz(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
+class TrolleSchwartzTest(unittest.TestCase):
+
+    def test_functionality(self):
+        t_start = 0
+        t_end = 1
+        r = 0.1
+        alpha_0 = 0.2
+        alpha_1 = 0.3
+        gamma = 0.4
+        kappa = 0.5
+        theta = 0.6
+        sigma = 0.7
+        rho = -0.5
+        n_steps = 1000
+        n_paths = 3
+        trolle_schwartz = TrolleSchwartz(t_start, t_end, r, alpha_0, alpha_1,
+                                         gamma, kappa, theta, sigma, rho, 'absolute_euler')
+        try:
+            trolle_schwartz.generate_scenarios(n_paths=n_paths, n_steps=n_steps)
+            trolle_schwartz.plot_underlying()
+            self.assertTrue(True)
+        except Exception as e:
+            self.fail(f"Error {e} occured during computation of solution path")
+
+    @unittest.skip("For visual inspection only")
+    def test_plot(self):
+        t_start = 0
+        t_end = 1
+        r = 0.1
+        alpha_0 = 0.2
+        alpha_1 = 0.3
+        gamma = 0.4
+        kappa = 0.5
+        theta = 0.6
+        sigma = 0.7
+        rho = -0.5
+        n_steps = 1000
+        n_paths = 3
+        trolle_schwartz = TrolleSchwartz(t_start, t_end, r, alpha_0, alpha_1,
+                                         gamma, kappa, theta, sigma, rho, 'absolute_euler')
+        trolle_schwartz.generate_scenarios(n_paths=n_paths, n_steps=n_steps)
+        trolle_schwartz.plot_underlying()
 
 if __name__ == '__main__':
     unittest.main()
